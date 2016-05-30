8cc618bf9d98755e7962891e76433113b5eb3eac
Fellipe Couto <fellipecouto96@gmail.com>
2016-05-30 15:06:39 -0300

    Cadastro de novo usuário

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,38 +1,41 @@
 angular.module('app.controllers', [])
-  
+
 .controller('loginCtrl', function($scope) {
 
 })
-   
-.controller('cadastroCtrl', function($scope) {
 
-})
-   
 .controller('homeCtrl', function($scope) {
 
 })
-   
+
 .controller('atendimentoCtrl', function($scope) {
 
 })
-   
+
 .controller('divisOFinanceiraCtrl', function($scope) {
 
 })
-   
+
 .controller('secretRiaAcadMicaCtrl', function($scope) {
 
 })
-   
+
 .controller('filaDeEsperaCtrl', function($scope) {
 
 })
-   
+
 .controller('atendimentoDaFilaCtrl', function($scope) {
 
 })
-   
+
 .controller('telaDeAgradecimentoCtrl', function($scope) {
 
 })
- 
+
+.controller('cadastroCtrl', function($scope) {
+  $scope.usuarios = [];
+
+  $scope.cadastrar = function (usuario) {
+    $scope.usuarios.push(usuario);
+  }
+})
