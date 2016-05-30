90f2593ddeb358b175295eface3f1bf08fadc01b
Fellipe Couto <fellipecouto96@gmail.com>
2016-05-30 01:04:57 -0300

    Upload Templates via Ionic Creator

---
+++ www/js/controllers.js
@@ -0,0 +1,34 @@
+angular.module('app.controllers', [])
+  
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('atendimentoCtrl', function($scope) {
+
+})
+   
+.controller('divisOFinanceiraCtrl', function($scope) {
+
+})
+   
+.controller('secretRiaAcadMicaCtrl', function($scope) {
+
+})
+   
+.controller('filaDeEsperaCtrl', function($scope) {
+
+})
+   
+.controller('atendimentoDaFilaCtrl', function($scope) {
+
+})
+   
+.controller('telaDeAgradecimentoCtrl', function($scope) {
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,65 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('home', {
+    url: '/home',
+    templateUrl: 'templates/home.html',
+    controller: 'homeCtrl'
+  })
+
+  .state('atendimento', {
+    url: '/atendimento',
+    templateUrl: 'templates/atendimento.html',
+    controller: 'atendimentoCtrl'
+  })
+
+  .state('divisOFinanceira', {
+    url: '/financeiro',
+    templateUrl: 'templates/divisOFinanceira.html',
+    controller: 'divisOFinanceiraCtrl'
+  })
+
+  .state('secretRiaAcadMica', {
+    url: '/secretaria',
+    templateUrl: 'templates/secretRiaAcadMica.html',
+    controller: 'secretRiaAcadMicaCtrl'
+  })
+
+  .state('filaDeEspera', {
+    url: '/espera',
+    templateUrl: 'templates/filaDeEspera.html',
+    controller: 'filaDeEsperaCtrl'
+  })
+
+  .state('atendimentoDaFila', {
+    url: '/chamada',
+    templateUrl: 'templates/atendimentoDaFila.html',
+    controller: 'atendimentoDaFilaCtrl'
+  })
+
+  .state('telaDeAgradecimento', {
+    url: '/agradecimento',
+    templateUrl: 'templates/telaDeAgradecimento.html',
+    controller: 'telaDeAgradecimentoCtrl'
+  })
+
+$urlRouterProvider.otherwise('/login')
+
+  
+
+});
